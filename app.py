2f10c1350a5390d782a9eda7b3bfb478177db052
socramtv <75081596+socramtv@users.noreply.github.com>
2026-08-10 18:12:13 +0200

    Actualizar app.py

--- app.py
+++ app.py
@@ -262,10 +262,11 @@ def obtener_datos_completos():
             eventos[i]['equipo_local'] = partes[0].strip() if len(partes) >= 1 else eventos[i]['equipos']
             eventos[i]['equipo_visitante'] = partes[1].strip() if len(partes) == 2 else ""
             
+        # Limpiamos los logos por defecto. Si no hay, se queda vacío para no mostrar el interrogante
         if not eventos[i].get('logo_local'):
-            eventos[i]['logo_local'] = "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23555'><path d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H7c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.04-.42 1.99-1.07 2.75z'/></svg>"
+            eventos[i]['logo_local'] = ""
         if not eventos[i].get('logo_visitante'):
-            eventos[i]['logo_visitante'] = "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23555'><path d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H7c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.04-.42 1.99-1.07 2.75z'/></svg>"
+            eventos[i]['logo_visitante'] = ""
 
         # MARCAR SI TIENE ENLACE ACTIVO
         eventos[i]['has_links'] = 'btn-canal' in eventos[i]['canales_html']
